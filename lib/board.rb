452c3df35356fbf88b2ae00ce2383fcf803d3dc5
Roy Vlasman <r.vlasman@ospcatering.nl>
2020-10-07 20:24:30 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,29 @@
 # Define the variable board below.
+row = ["0", "1", "2"]
+ 
+# Position 1: Left
+row[0]
+ 
+# Position 2: Middle
+row[1]
+ 
+# Position 3: Right
+row[2]
+ 
+ # Position 4: Left
+row[0]
+
+# Position 5: Middle
+row[1]
+ 
+# Position 6: Right
+row[2]
+
+ # Position 7: Left
+row[0]
+
+# Position 8: Middle
+row[1]
+ 
+# Position 9: Right
+row[2]
